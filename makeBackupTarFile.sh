cb6398cde54ce105d6de849dce2b53b5bae1fb52
Charlie Duke <duke@duke.grinnell.edu>
2012-09-29 10:36:23 -0500

    cd: deleting makeBackupTarFile in grOptics - still in ./scripts

--- makeBackupTarFile.sh
+++
@@ -1,9 +0,0 @@
-DATEM="28Sept3PM"
-
-tar -cvf grOptics$DATEM.tar ./src ./include ./README ./scripts
-
-gzip grOptics$DATEM.tar
-
-scp grOptics$DATEM.tar.gz duke@ampere.math.grin.edu:~/GrOpticsBAK/.
-
-rm grOptics$DATEM.tar.gz
