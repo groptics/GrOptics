b1ea44db0fd6c21fe3fb9fdf5efe13ec43692e03
Charlie Duke <duke@duke.grinnell.edu>
2012-10-02 09:54:09 -0500

    cd:  adding html doc. files. plus mainpage.h file

--- README/latex2html.sh
+++ README/latex2html.sh
@@ -3,8 +3,23 @@
 #   Charlie Duke
 #   Grinnell College
 #   September 28, 2012
-plastex --split-level=-2 --dir=./ --filename=UsersGuideGrOptics.html UsersGuideGrOptics.tex
 
-rm -rf UsersGuideGrOptics
+# this script produces both a single html file:
+#         GrOptics/README/UsersGuideGrOptics.html
+# and linked chapter html files:
+#         GrOptics/README/html/index.html
+
+plastex  --dir=./ --renderer=XHTML --dir=./html UsersGuideGrOptics.tex
+rm html/*.jhm html/*.xml html/*.hs html/*.paux html/*.hh* 
+rm -rf html/icons html/images html/styles
+
+plastex --split-level=-4 --dir=./ --filename=UsersGuideGrOptics.html UsersGuideGrOptics.tex
+
 rm *.jhm *.xml *.hs *.paux *.hh* 
 rm -rf icons images styles
+
+# lastly, produce a pdf file from UsersGuideGrOptics.tex
+
+latex UsersGuideGrOptics
+dvipdf UsersGuideGrOptics.dvi
+rm UsersGuideGrOptics.aux UsersGuideGrOptics.dvi UsersGuideGrOptics.log
---
+++ include/mainpage.h
@@ -0,0 +1,3 @@
+/*! \mainpage this is the main page
+<a href="/home/duke/Development/GrOpticsGitLocal/GrOptics/README/html/index.html">link UsersGuideGrOptics</a>*/
+*/
