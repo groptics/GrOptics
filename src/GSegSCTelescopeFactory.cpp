447001e0933fe908e536c78bef387d714879c628
Charles Duke <duke@Charles-Dukes-MacBook-Pro.local>
2012-12-22 13:21:22 -0600

    files from gamow to laptop to repository

--- src/GSegSCTelescopeFactory.cpp
+++ src/GSegSCTelescopeFactory.cpp
@@ -199,18 +199,18 @@ void SegSCStdOptics::printSegSCStdOptics() {
     
 };
 /************** end of printSegSCStdOptics ***********************/
-void SegSCStdOptics::printSegVector (const vector<mirrorSegmentDetails> &vec) {
+void SegSCStdOptics::printSegVector (const vector<mirrorSegmentDetails *> &vec) {
   *oLog << "        num rmin    rmax marg dPhi refl rough errXYZ rotErrABC"
         << endl;
   int numElem = vec.size();
   for (int i = 0;i<numElem; i++) {
-    mirrorSegmentDetails t = vec[i];
+    mirrorSegmentDetails *t = vec[i];
     *oLog << "         " << i+1 << "  " 
-          << t.rmin << "  " << t.rmax << "  " << t.margin << "  " 
-          << t.delPhi << "  " << t.reflect << "   " << t.roughness
-          << "      " << t.posErrorX
-          << " " << t.posErrorY << " " << t.posErrorZ << "    " 
-          << t.rotErrorX << " " << t.rotErrorY << " " << t.rotErrorZ
+          << t->rmin << "  " << t->rmax << "  " << t->margin << "  " 
+          << t->delPhi << "  " << t->reflect << "   " << t->roughness
+          << "      " << t->posErrorX
+          << " " << t->posErrorY << " " << t->posErrorZ << "    " 
+          << t->rotErrorX << " " << t->rotErrorY << " " << t->rotErrorZ
           << endl;
   }
 
@@ -272,21 +272,99 @@ GSegSCTelescopeFactory::~GSegSCTelescopeFactory() {
 GSegSCTelescope* GSegSCTelescopeFactory::makeTelescope(const int &id,
                                                      const int &std) {
   
-  int debug = 1;
-  if (debug>0) {
+  int debug = true;
+  if (debug) {
     *oLog << " -- GSegSCTelescopeFactory::makeTelescope" << endl;
     //*oLog << "      telID  = " << id << endl;
     //*oLog << "      telStd = " << std << endl;
   }
+
+  Int_t idTel = id;
+  Int_t iStdID = std;
+
+  // get parameters for this telescope
+  itmStdOp = mStdOptics.find(iStdID);
+  assert(itmStdOp != mStdOptics.end());
+  // make pointer to working stdoptics structure, easier typing
+  opt = mStdOptics[iStdID];
+
+  // make the telescope
   SCTel = new GSegSCTelescope;
+
+  // move over all reflection coefficients (the entire map)
+  SCTel->setReflCoeffMap(mGRefl);
+  
+  SCTel->setTelID(idTel);
+  SCTel->setStdID(iStdID);
+  SCTel->eTelType = opt->stdType;
+  SCTel->fAvgTransitTime = opt->fAvgTransitTime;
+  SCTel->fRotationOffset = opt->fRotationOffset;
+
+  // general telescope parameters
+  SCTel->fF = opt->fF;
+  SCTel->fAlpha = opt->fAlpha;
+  SCTel->fQ = opt->fQ;
+
+  // primary parameters
+  SCTel->fRpMax = opt->fRpMax;
+  SCTel->fRpMin = opt->fRpMin;
+  SCTel->fZp = opt->fZp;
+  SCTel->fNp = opt->fNp;
+  SCTel->iNParP = opt->iNParP;
+  SCTel->fzp = opt->fzp;
+  // make primary poly coefficients
+  SCTel->fP = new Double_t[SCTel->iNParP];
+  Double_t fF = opt->fF;
+  (SCTel->fP)[0] = TMath::Power(fF,  1)* ((opt->fzp[0]));
+  (SCTel->fP)[1] = TMath::Power(fF,  -1)* ((opt->fzp[1]));
+  (SCTel->fP)[2] = TMath::Power(fF,  -3)* ((opt->fzp[2]));
+  (SCTel->fP)[3] = TMath::Power(fF,  -5)* ((opt->fzp[3]));
+  (SCTel->fP)[4] = TMath::Power(fF,  -7)* ((opt->fzp[4]));
+  (SCTel->fP)[5] = TMath::Power(fF,  -9)* ((opt->fzp[5]));
+  (SCTel->fP)[6] = TMath::Power(fF,  -11)* ((opt->fzp[6]));
+  (SCTel->fP)[7] = TMath::Power(fF,  -13)* ((opt->fzp[7]));
+  
+  // secondary parameters
+  SCTel->fRsMax = opt->fRsMax;
+  SCTel->fRsMin = opt->fRsMin;
+  SCTel->fZs = opt->fZs;
+  SCTel->fNs = opt->fNs;
+  SCTel->fS = opt->fS;
+  SCTel->iNParS = opt->iNParS;
+  SCTel->fzs = opt->fzs;
+  // make secondary poly coefficients
+  SCTel->fS = new Double_t[SCTel->iNParS];
+  fF = opt->fF;
+  (SCTel->fS)[0] = TMath::Power(fF,  1)* ((opt->fzs[0]));
+  (SCTel->fS)[1] = TMath::Power(fF,  -1)* ((opt->fzs[1]));
+  (SCTel->fS)[2] = TMath::Power(fF,  -3)* ((opt->fzs[2]));
+  (SCTel->fS)[3] = TMath::Power(fF,  -5)* ((opt->fzs[3]));
+  (SCTel->fS)[4] = TMath::Power(fF,  -7)* ((opt->fzs[4]));
+  (SCTel->fS)[5] = TMath::Power(fF,  -9)* ((opt->fzs[5]));
+  (SCTel->fS)[6] = TMath::Power(fF,  -11)* ((opt->fzs[6]));
+  (SCTel->fS)[7] = TMath::Power(fF,  -13)* ((opt->fzs[7]));
+
+  // primary segment details
+  SCTel->iNumP1Mirrors = opt->iNumP1Mirrors;
+  SCTel->iNumP2Mirrors = opt->iNumP2Mirrors;
+  SCTel->vSegP1 = opt->vSegP1;
+  SCTel->vSegP2 = opt->vSegP2;
+
+ // secondary segment details
+  SCTel->iNumS1Mirrors = opt->iNumS1Mirrors;
+  SCTel->vSegS1 = opt->vSegS1;
+  SCTel->vSegS2 = opt->vSegS2;
+  SCTel->printTelescope();
+  SCTel->buildTelescope();
+
   return SCTel;
  
 };
 /************** end of makeTelescope ***********************/
 
 void GSegSCTelescopeFactory::editWorkingTelescope(GSegSCTelescope *SCTel1) {
-  int debug = 1;
-  if (debug>0) {
+  bool debug = true;
+  if (debug) {
     *oLog << " -- GSegSCTelescopeFactory::editWorkingTelescope" << endl;
   }
 
